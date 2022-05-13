d875c3a3d7239bbf9a9cc1ddc872f267f712b6f8
Yee Li <seven.yi.lee@gmail.com>
2022-05-13 11:30:30 +0800

    Delete .get_zeroed_page_test.mod.cmd

--- .get_zeroed_page_test.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/yeeli/Yee-HDD/2021_during_home/64bit_pml4/get_zeroed_page_4k/get_zeroed_page_test.mod := { echo  /home/yeeli/Yee-HDD/2021_during_home/64bit_pml4/get_zeroed_page_4k/get_zeroed_page_test.o;  echo; } > /home/yeeli/Yee-HDD/2021_during_home/64bit_pml4/get_zeroed_page_4k/get_zeroed_page_test.mod
